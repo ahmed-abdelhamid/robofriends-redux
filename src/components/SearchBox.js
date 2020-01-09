4d74872f25d5f31cd35114266094349a0190cf00
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2020-01-09 10:55:33 +0300

    Add icons for PWA

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
-    <div className='pa2'>
+    <div className="pa2">
+      <label htmlFor="search" hidden>
+        Search
+      </label>
       <input
-        className='pa3 ba b--green bg-lightest-blue'
-        type='search'
-        placeholder='search robots'
+        id="search"
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
         onChange={searchChange}
       />
     </div>
   );
-}
+};
 
 export default SearchBox;
